6c6e0a5d4bba5568054caa5e52de03a4f3b34166
Renee Silva <rtsilva@mit.edu>
2020-04-20 15:48:51 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+"""6.835 Final Project Spring 2020"""
+# Renee Silva, Magdalena Price
+
+import sys
+import matplotlib
+
+def menu():
+    #put songs availible to sing here
+    return
+
+def play_song(): #
+    #show lyric video, play song, capture audio,
+    return
+
+
+def song_visual(): #lena
+    return
+
+def song_audio(): #renee
+    return
+
+
+def result():
+    #what the user should see after finishing song, button back to menu()
+    return
+
+
+if __name__ == '__main__':
+    pass
