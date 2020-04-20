06bd81b0763f635da1af2af719f3247868a5669b
Renee Silva <rtsilva@mit.edu>
2020-04-20 19:58:11 -0400

    should work just no audio detected

--- capture_audio.py
+++ capture_audio.py
@@ -5,41 +5,58 @@ few samples and play them back immediately).
 This is the callback (non-blocking) version.
 """
 import pyaudio
-import wave
 import time
-import struct
 
-CHUNK = 1024
+import aubio
+import numpy as np
+
+from audiolazy import freq2midi
+
+
+CHUNK = 2048 #1024
 FORMAT = pyaudio.paFloat32
 WIDTH = 2
 CHANNELS = 1
 RATE = 44100
+###
+HOP_SIZE                = CHUNK//2
+PERIOD_SIZE_IN_FRAME    = HOP_SIZE
+METHOD                  = "default"
 
-# instantiate PyAudio
+
+# instantiate PyAudio object
 p = pyaudio.PyAudio()
 
-def callback(in_data, frame_count, time_info, status):
-    # print(struct.unpack(str(1024)+'B', in_data))
-    return (in_data, pyaudio.paContinue)
-
-# open stream
-#format=p.get_format_from_width(WIDTH),
-stream = p.open(format=FORMAT,
+# open mic stream
+mic = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
-                output=True)#,
-                #stream_callback=callback)
+                frames_per_buffer=PERIOD_SIZE_IN_FRAME)
 
-stream.start_stream()
+# Initiating Aubio's pitch detection object.
+pDetection = aubio.pitch(METHOD, CHUNK, HOP_SIZE, RATE)
+# Set unit.
+pDetection.set_unit("Hz")
+# Frequency under 5 dB will considered
+# as a silence (8 dB is a C1 or midi#0)
+pDetection.set_silence(-40)
 
-while stream.is_active(): #while song is not over
-    data = stream.read(CHUNK)
-    print(len(data))
-    print(struct.unpack('4096B', data))
-    time.sleep(0.1)
 
-stream.stop_stream()
-stream.close()
+while mic.is_active():
+    # Always listening to the microphone.
+    data = mic.read(PERIOD_SIZE_IN_FRAME)
+    print(data)
+    # Convert into number that Aubio understand.
+    samples = np.fromstring(data, dtype=aubio.float_type)
+    # Finally get the pitch.
+    pitch = pDetection(samples)[0]
+
+    midi = freq2midi(pitch)
+
+    print(str(pitch)+" "+str(midi))
+
+mic.stop_stream()
+mic.close()
 
 p.terminate()
