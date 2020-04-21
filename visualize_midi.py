2fdb281d0f8fc20adb2c78a94cd0f9ca5ea1b0a5
Magdalena Price <maprice@mit.edu>
2020-04-20 22:58:05 -0500

    slightly better vers

--- visualize_midi.py
+++ visualize_midi.py
@@ -73,7 +73,8 @@ def init_midi(filename):
     ax.axes.yaxis.set_visible(False)
     x = []
     y = []
-    line, = ax.plot(x, y, 'bs', markersize=20)
+    line_one, = ax.plot(x, y, 'bs', markersize=20)
+    line_two, = ax.plot([], [], 'ro', markersize =20)
 
     # add note lines for reference, can remove later
     plt.plot([0, x_r], [30, 30], 'k-', lw=3)
@@ -84,66 +85,99 @@ def init_midi(filename):
     plt.plot([0, x_r], [5, 5], 'k-', lw=3)
     plt.plot([10, 10], [0, 35], 'r-', lw=3)
 
-    return fig, x_data, y_data, line
+    return fig, x_data, y_data, line_one, line_two
 
-def turn_mic_off(p, mic):
+# def turn_mic_off(p, mic):
+#     mic.stop_stream()
+#     mic.close()
+#     p.terminate()
+# 
+# def animate_user():
+#     ax = plt.axes(xlim=(0, x_r), ylim=(0, 35))
+#     other_line, = ax.plot([], [], 'ro', markersize =20)
+#
+#     def init():
+#         other_line.set_data([10], [0])
+#         return other_line,
+#
+#     def animate(i):
+#         # instantiate PyAudio object
+#         p = pyaudio.PyAudio()
+#
+#         new_x = [10]
+#
+#         # open mic stream
+#         mic = p.open(format=FORMAT,
+#                         channels=CHANNELS,
+#                         rate=RATE,
+#                         input=True,
+#                         frames_per_buffer=PERIOD_SIZE_IN_FRAME)
+#
+#         # Initiating Aubio's pitch detection object.
+#         pDetection = aubio.pitch(METHOD, CHUNK, HOP_SIZE, RATE)
+#         # Set unit.
+#         pDetection.set_unit("Hz")
+#         # Frequency under 5 dB will considered
+#         # as a silence (8 dB is a C1 or midi#0)
+#         pDetection.set_silence(-40)
+#
+#         data = mic.read(PERIOD_SIZE_IN_FRAME)
+#         # Convert into number that Aubio understand.
+#         samples = np.fromstring(data, dtype=aubio.float_type)
+#         # Finally get the pitch.
+#         pitch = pDetection(samples)[0]
+#
+#         midi = freq2midi(pitch)
+#
+#         new_y = [midi%12]
+#
+#         other_line.set_data(new_x, new_y)
+#
+#         mic.stop_stream()
+#         mic.close()
+#         p.terminate()
+#
+#         return other_line,
+#
+#     anim = FuncAnimation(fig, animate, init_func=init,
+#                                frames=1, interval=1, blit=True, repeat = True)
+#
+#     return plt
+
+def get_user_audio():
+    # instantiate PyAudio object
+    p = pyaudio.PyAudio()
+
+    # open mic stream
+    mic = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=PERIOD_SIZE_IN_FRAME)
+
+    # Initiating Aubio's pitch detection object.
+    pDetection = aubio.pitch(METHOD, CHUNK, HOP_SIZE, RATE)
+    # Set unit.
+    pDetection.set_unit("Hz")
+    # Frequency under 5 dB will considered
+    # as a silence (8 dB is a C1 or midi#0)
+    pDetection.set_silence(-40)
+
+    data = mic.read(PERIOD_SIZE_IN_FRAME)
+    # Convert into number that Aubio understand.
+    samples = np.fromstring(data, dtype=aubio.float_type)
+    # Finally get the pitch.
+    pitch = pDetection(samples)[0]
+
+    midi = freq2midi(pitch)
+
     mic.stop_stream()
     mic.close()
     p.terminate()
 
-def animate_user():
-    ax = plt.axes(xlim=(0, x_r), ylim=(0, 35))
-    other_line, = ax.plot([], [], 'ro', markersize =20)
+    return midi
 
-    def init():
-        other_line.set_data([10], [0])
-        return other_line,
-
-    def animate(i):
-        # instantiate PyAudio object
-        p = pyaudio.PyAudio()
-
-        new_x = [10]
-
-        # open mic stream
-        mic = p.open(format=FORMAT,
-                        channels=CHANNELS,
-                        rate=RATE,
-                        input=True,
-                        frames_per_buffer=PERIOD_SIZE_IN_FRAME)
-
-        # Initiating Aubio's pitch detection object.
-        pDetection = aubio.pitch(METHOD, CHUNK, HOP_SIZE, RATE)
-        # Set unit.
-        pDetection.set_unit("Hz")
-        # Frequency under 5 dB will considered
-        # as a silence (8 dB is a C1 or midi#0)
-        pDetection.set_silence(-40)
-
-        data = mic.read(PERIOD_SIZE_IN_FRAME)
-        # Convert into number that Aubio understand.
-        samples = np.fromstring(data, dtype=aubio.float_type)
-        # Finally get the pitch.
-        pitch = pDetection(samples)[0]
-
-        midi = freq2midi(pitch)
-
-        new_y = [midi%12]
-
-        other_line.set_data(new_x, new_y)
-
-        mic.stop_stream()
-        mic.close()
-        p.terminate()
-
-        return other_line,
-
-    anim = FuncAnimation(fig, animate, init_func=init,
-                               frames=1, interval=1, blit=True, repeat = True)
-
-    return plt
-
-def animate_midi(fig, x_data, y_data, line):
+def animate_midi(fig, x_data, y_data, line_one,line_two):
     '''
     input:
         - fig: where animation will be shown
@@ -156,8 +190,9 @@ def animate_midi(fig, x_data, y_data, line):
     y_data_space = [(0,0) for i in range(x_r)] + y_data + [(0,0) for i in range(x_r)]
 
     def init():
-        line.set_data([i for i in x_range], [0 for i in range(x_r*2)])
-        return line,
+        line_one.set_data([i for i in x_range], [0 for i in range(x_r*2)])
+        line_two.set_data([10], [0])
+        return line_one, line_two
 
     def animate(i):
         k = i
@@ -175,17 +210,22 @@ def animate_midi(fig, x_data, y_data, line):
                 new_x.append(x[i])
                 new_y.append(y[i][0])
 
-        line.set_data(new_x, new_y)
-        return line,
+        midi = get_user_audio()
+
+        line_one.set_data(new_x, new_y)
+
+        line_two.set_data([10], [midi%12])
+
+        return line_one, line_two
 
     # frames is number of notes we are showing, interval is time(ms) between each note
     anim = FuncAnimation(fig, animate, init_func=init,
                                frames=len(x_data) + x_r, interval=1, blit=True, repeat = False)
 
     # plt.show()
-    return None
+    return plt
 
-# fig, x_data, y_data, line = init_midi("twinkle-twinkle-little-star.mid")
-# animate_midi(fig, x_data, y_data, line)
+fig, x_data, y_data, line_one, line_two = init_midi("twinkle-twinkle-little-star.mid")
+plt = animate_midi(fig, x_data, y_data, line_one, line_two)
 # plt = animate_user()
-# plt.show()
+plt.show()
