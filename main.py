b253bda5b6dd22633c29c85d06552d71f0664611
Magdalena Price <maprice@mit.edu>
2020-05-04 20:20:15 -0500

    starting to transfer to pygame

--- main.py
+++ main.py
@@ -3,28 +3,58 @@
 
 import sys
 import matplotlib
+import pygame
+# pygame.init()
 
 from app import KaraokeHeroApp
 
-def menu():
-    #put songs availible to sing here
-    return
+class App:
+    user_id = None
+    song_selection = None
+    is_playing = False
+    quit = False
+    score = None
+    # completed_song = False
 
-def play_song(): #
-    #show lyric video, play song, capture audio,
-    return
+    width = 200
+    height = 100
+    button_w = 40
+    button_h = 20
+    screen_w = 100
+    screen_h = 50
 
-def song_visual(): #lena
-    return
+    def __init__(self, user_id):
+        self.user_id = user_id
 
-def song_audio(): #renee
-    return
 
-def result():
-    #what the user should see after finishing song, button back to menu()
-    return
+    def menu(self):
+        # display available songs and return selected song if song chosen
+        # else False (user has chosen to quit)
+        return
+
+    def play_song(self): #
+        # show lyric video, play song, capture audio
+        # return True if song finishes else False
+        # updates score
+        return
+
+    def upload_score(self):
+        # uploads user's score
+        return
+
+    def song_visual(self): #lena
+        return
+
+    def song_audio(self): #renee
+        return
+
+    def result(self):
+        # what the user should see after finishing song, button back to menu()
+        return
 
 
 if __name__ == '__main__':
-    app = KaraokeHeroApp()
-    app.mainloop()
+    # app = KaraokeHeroApp()
+    # app.mainloop()
+    pygame.init()
+    app = App(1)
