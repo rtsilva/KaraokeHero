22941268d2a216b5cc1226561f8f9557584ea0a6
Magdalena Price <maprice@mit.edu>
2020-05-06 16:34:17 -0500

    minor style change

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     cont = True
     while cont:
         song = app.menu()
-        if song == False:
+        if not song:
             app.quit()
             break
         else:
